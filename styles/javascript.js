8e16ab938626efe57209376e8b2c1658e62992cd
palgunovaliza <55555608+palgunovaliza@users.noreply.github.com>
2020-03-08 20:28:15 +0300

    covv

--- styles/javascript.js
+++ styles/javascript.js
@@ -1,13 +1,43 @@
 
 let restangle = anime({
   targets: '.restangle1',
-  height: 110,
+  height: 100,
   autoplay: false,
   duration: 200,
   easing:'easeInSine'
+
 })
 
 let restangle1Click =
 document.querySelector('.restangle1')
 
 restangle1Click.onclick = restangle.play
+
+let restanglered = anime({
+  targets: '.restangle3',
+  height: 100,
+  autoplay: false,
+  duration: 200,
+  top: 1444,
+  easing:'easeInSine'
+})
+
+let restangle1Click2 =
+document.querySelector('.restangle3')
+
+restangle1Click2.onclick = restanglered.play
+
+let restangleblue = anime({
+  targets: '.restangle4',
+  height: 100,
+  autoplay: false,
+  duration: 200,
+  top: 1543,
+  easing:'easeInSine'
+})
+
+
+let restangle1Click3 =
+document.querySelector('.restangle4')
+
+restangle1Click3.onclick = restangleblue.play
