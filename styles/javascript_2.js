0dc7b3bd8d18d8b583e7cd831e7fbca7db7af76b
palgunovaliza <55555608+palgunovaliza@users.noreply.github.com>
2020-03-16 18:29:53 +0300

    comm

--- styles/javascript_2.js
+++ styles/javascript_2.js
@@ -2,10 +2,15 @@ let tl = anime.timeline({
   autoplay: false
 })
 
-tl
   .add({
     targets: '.redyellow',
     top: ['27px','-115px'],
+    easing:'easeInSine',
+    duration: 800,
+  }, 0)
+  .add({
+    targets: '.ellipseyellow',
+    top: ['202px','400px'],
     easing:'easeInSine',
     duration: 800,
   }, 0)
@@ -16,11 +21,72 @@ tl
     duration: 800,
   }, 0)
 
+let yellowredClick =
+document.querySelector('.redyellow')
+yellowredClick.onclick = tl.play
+
+
+let tl2 = anime.timeline({
+  autoplay: false
+})
+
+  .add({
+    targets: '.ellipseblue',
+    top: ['521px','760px'],
+    easing:'easeInSine',
+    duration: 800,
+  }, 0)
+  .add({
+    targets: '.ellipseyellow2',
+    top: ['780px','600px'],
+    easing:'easeInSine',
+    duration: 800,
+  }, 0)
+
+let blueClick =
+document.querySelector('.ellipseblue')
+blueClick.onclick = tl2.play
+
+
+let tl3 = anime.timeline({
+  autoplay: false
+})
+
+  .add({
+    targets: '.ellipsered',
+    top: ['1025px','1170px'],
+    easing:'easeInSine',
+    duration: 800,
+  }, 0)
+  .add({
+    targets: '.blueyellow',
+    top: ['1135px','925px'],
+    easing:'easeInSine',
+    duration: 800,
+  }, 0)
+
+let redClick =
+document.querySelector('.ellipsered')
+redClick.onclick = tl3.play
+
+
+let tl4 = anime.timeline({
+  autoplay: false
+})
+
+  .add({
+    targets: '.planetyellow',
+    top: ['1190px','1500px'],
+    easing:'easeInSine',
+    duration: 800,
+  }, 0)
+  .add({
+    targets: '.ellipsered2',
+    top: ['1500px','1378px'],
+    easing:'easeInSine',
+    duration: 800,
+  }, 0)
 
 let planetyellowClick =
-document.querySelector('.redyellow')
-planetyellowClick.onclick = tl.play
-
-let blueredClick =
-document.querySelector('.bluered')
-blueredClick.onclick = tl.play
+document.querySelector('.planetyellow')
+planetyellowClick.onclick = tl4.play
