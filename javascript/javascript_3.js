3a8ae7b31ed0aa36ba51c69c00d812b4e8964943
palgunovaliza <55555608+palgunovaliza@users.noreply.github.com>
2020-03-18 20:08:22 +0300

    сои

---
+++ javascript/javascript_3.js
@@ -0,0 +1,92 @@
+anime({
+  targets: '.square1',
+  height: 10,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+});
+
+anime({
+  targets: '.square2',
+  width: 5,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+});
+anime({
+  targets: '.square3',
+  height: 7,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+});
+anime({
+  targets: '.square4',
+  width: 20,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+});
+anime({
+  targets: '.square5',
+  height: 5,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+});
+anime({
+  targets: '.square6',
+  width: 15,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+});
+anime({
+  targets: '.square7',
+  width: 5,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+});
+anime({
+  targets: '.square8',
+  height: 20,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+});
+anime({
+  targets: '.square9',
+  width: 10,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+});
+anime({
+  targets: '.square10',
+  height: 5,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+});
+anime({
+  targets: '.square11',
+  height: 17,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+});
+anime({
+  targets: '.square12',
+  width: 7,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+});
+anime({
+  targets: '.square13',
+  width: 9,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+});
