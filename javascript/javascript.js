bf18f84dbc846c9425c96ee04f62cd531f2fe130
palgunovaliza <55555608+palgunovaliza@users.noreply.github.com>
2020-03-22 15:46:44 +0600

    com

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,164 +1,199 @@
-// let redyellow = anime ({
-// targets: '.redyellow',
-// top: ['27px','-110px'],
-// easing:'easeInSine',
-// duration: 900,
-// autoplay: false
+// let restangleYellow = anime({
+//   targets: '.restangle1',
+//   height: 100,
+//   autoplay: false,
+//   duration: 200,
+//   easing:'easeInSine'
+//
 // })
 //
-// let redyellowonclick =
-// document.querySelector('.redyellow')
-// redyellowonclick.onclick = redyellow.play
 //
-// let ellipseyellow = anime ({
-// targets: '.ellipseyellow',
-// top: ['214px','400px'],
-// easing:'easeInSine',
-// duration: 900,
-// autoplay: false
+// let restangleClick =
+// document.querySelector('.restangle1')
+//
+// restangleClick.onclick = restangleYellow.play
+//
+// let restangleRed = anime({
+//   targets: '.restangle3',
+//   height: 100,
+//   autoplay: false,
+//   duration: 200,
+//   top: 1539,
+//   easing:'easeInSine'
 // })
 //
-// let ellipseyellowonclick =
-// document.querySelector('.ellipseyellow')
-// ellipseyellowonclick.onclick = ellipseyellow.play
+// let restangleClick3 =
+// document.querySelector('.restangle3')
 //
+// restangleClick3.onclick = restangleRed.play
 //
-// let ellipseblue = anime ({
-// targets: '.ellipseblue',
-// top: ['520px','750px'],
-// easing:'easeInSine',
-// duration: 900,
-// autoplay: false
+// let restangleBlue = anime({
+//   targets: '.restangle4',
+//   height: 100,
+//   autoplay: false,
+//   duration: 200,
+//   top: 1640,
+//   easing:'easeInSine'
 // })
 //
-// let ellipseblueonclick =
-// document.querySelector('.ellipseblue')
-// ellipseblueonclick.onclick = ellipseblue.play
+//
+// let restangleClick4 =
+// document.querySelector('.restangle4')
+//
+// restangleClick4.onclick = restangleBlue.play
 //
 //
-// let ellipsered = anime ({
-// targets: '.ellipsered',
-// top: ['1000px','1120px'],
-// easing:'easeInSine',
-// duration: 900,
-// autoplay: false
+// let restangleBlack = anime({
+//   targets: '.restangle2',
+//   transparency: '0',
+//
 // })
 //
-// let ellipseredonclick =
-// document.querySelector('.ellipsered')
-// ellipseredonclick.onclick = ellipsered.play
+// let restangleClick2 =
+// document.querySelector('.restangle2')
 //
-// let planetyellow = anime ({
-// targets: '.planetyellow',
-// top: ['1190px','1500px'],
-// easing:'easeInSine',
-// duration: 900,
-// autoplay: false
-// })
-//
-// let planetyellowonclick =
-// document.querySelector('.planetyellow')
-// planetyellowonclick.onclick = planetyellow.play
-
+// restangleClick2.onclick = restangleBlack.play
 
 let tl = anime.timeline({
   autoplay: false
 })
 
-tl
   .add({
-    targets: '.redyellow',
-    top: ['27px','-115px'],
-    easing:'easeInSine',
-    duration: 800,
-
-  })
-  .add({
-    targets: '.ellipseyellow',
-    top: ['202px','400px'],
-    easing:'easeInSine',
-    duration: 800,
+    targets: '.restangle1',
+    display: 'none',
+    opacity: 0,
+    duration: 10,
   }, 0)
   .add({
-    targets: '.ellipseblue',
-    top: ['521px','760px'],
+    targets: '.restangle2',
+    height: 100,
+    top: 1442,
     easing:'easeInSine',
-    duration: 800,
+    duration: 300,
   }, 0)
   .add({
-    targets: '.ellipsered',
-    top: ['1025px','1170px'],
+    targets: '.restangle3',
+    height: 100,
+    top: 1541,
     easing:'easeInSine',
-    duration: 800,
+    duration: 300,
   }, 0)
   .add({
-    targets: '.planetyellow',
-    top: ['1190px','1500px'],
+    targets: '.restangle4',
+    height: 100,
+    top: 1640,
     easing:'easeInSine',
-    duration: 800,
+    duration: 300,
+  }, 0)
+
+
+let restangle1Click =
+document.querySelector('.restangle1')
+restangle1Click.onclick = tl.play
+
+let tl2 = anime.timeline({
+  autoplay: false
+})
+  .add({
+    targets: '.restangle1',
+    height: 100,
+    easing:'easeInSine',
+    duration: 350,
   }, 0)
   .add({
-    targets: '.bluered',
-    top: ['-300px','-25px'],
+    targets: '.restangle2',
+    display: 'none',
+    opacity: 0,
+    duration: 10,
+  }, 0)
+  .add({
+    targets: '.restangle3',
+    height: 100,
+    top: 1541,
     easing:'easeInSine',
-    duration: 800,
-
+    duration: 300,
+  }, 0)
+  .add({
+    targets: '.restangle4',
+    height: 100,
+    top: 1640,
+    easing:'easeInSine',
+    duration: 300,
   }, 0)
 
-let redyellowClick =
-document.querySelector('.redyellow')
-redyellowClick.onclick = tl.play
 
-let ellipseyellowClick =
-document.querySelector('.ellipseyellow')
-ellipseyellowClick.onclick = tl.play
+let restangle2Click =
+document.querySelector('.restangle2')
+restangle2Click.onclick = tl2.play
 
-let ellipseblueClick =
-document.querySelector('.ellipseblue')
-ellipseblueClick.onclick = tl.play
+let tl3 = anime.timeline({
+  autoplay: false
+})
+  .add({
+    targets: '.restangle1',
+    height: 100,
+    easing:'easeInSine',
+    duration: 350,
+  }, 0)
+  .add({
+    targets: '.restangle2',
+    height: 100,
+    top: 100,
+    easing:'easeInSine',
+    duration: 350,
+  }, 0)
+  .add({
+    targets: '.restangle3',
+    display: 'none',
+    opacity: 0,
+    duration: 10,
+  }, 0)
+  .add({
+    targets: '.restangle4',
+    height: 100,
+    top: 1640,
+    easing:'easeInSine',
 
-let ellipseredClick =
-document.querySelector('.ellipsered')
-ellipseredClick.onclick = tl.play
-
-let planetyellowClick =
-document.querySelector('.planetyellow')
-planetyellowClick.onclick = tl.play
-
-let blueredClick =
-document.querySelector('.bluered')
-blueredClick.onclick = tl.play
+    duration: 300,
+  }, 0)
 
 
-// let planets = anime ({
-//   targets: '.redyellow',
-//   top: ['27px','-110px'],
-//   easing:'easeInSine',
-//   duration: 900,
-//   autoplay: false,
-//   targets: '.ellipseyellow',
-//   top: ['214px','400px'],
-//   easing:'easeInSine',
-//   duration: 900,
-//   autoplay: false,
-//   targets: '.ellipseblue',
-//   top: ['520px','750px'],
-//   easing:'easeInSine',
-//   duration: 900,
-//   autoplay: false,
-//   targets: '.ellipsered',
-//   top: ['1000px','1120px'],
-//   easing:'easeInSine',
-//   duration: 900,
-//   autoplay: false,
-//   targets: '.planetyellow',
-//   top: ['1103px','1500px'],
-//   easing:'easeInSine',
-//   duration: 900,
-//   autoplay: false,
-//
-// })
-//
-// let planetsClick =
-// document.querySelector('.redyellow')
-// planetsClick.onclick = planets.play
+let restangle3Click =
+document.querySelector('.restangle3')
+restangle3Click.onclick = tl3.play
+
+
+let tl4 = anime.timeline({
+  autoplay: false
+})
+  .add({
+    targets: '.restangle1',
+    height: 100,
+    easing:'easeInSine',
+    duration: 300,
+  }, 0)
+  .add({
+    targets: '.restangle2',
+    height: 100,
+    top: 100,
+    easing:'easeInSine',
+    duration: 300,
+  }, 0)
+  .add({
+    targets: '.restangle3',
+    height: 100,
+    top: 200,
+    easing:'easeInSine',
+    duration: 300,
+  }, 0)
+  .add({
+    targets: '.restangle4',
+    display: 'none',
+    opacity: 0,
+    duration: 10,
+  }, 0)
+
+
+let restangle4Click =
+document.querySelector('.restangle4')
+restangle4Click.onclick = tl4.play
---
+++ javascript/javascript_4.js
@@ -0,0 +1,62 @@
+document.getElementById('eyered1').onclick = function() {
+document.getElementById('eyeclose1').style.display = 'block'
+}
+
+
+document.getElementById('eyeclose1').onclick = function() {
+document.getElementById('eyered1').style.display = 'none'
+}
+// let tl1 = anime.timeline({
+//   autoplay: false,
+// })
+//   .add({
+//     targets: '.eyered1',
+//     easing: 'linear',
+//     opacity: ['100', '0'],
+//     duration: 10,
+//   }, 0)
+//   .add({
+//     targets: '.eyeclose1',
+//     easing: 'linear',
+//     opacity: ['0', '100'],
+//     duration: 300,
+//   }, 0)
+//
+//   // if ( display === true ) {
+//   // $( "eyered1" ).show('eyered1');
+//   // } else if ( display === false ) {
+//   // $( "eyeclose1" ).hide('eyeclose1');
+//   // }
+//
+// let eyeredClick =
+// document.querySelector('.eyered1')
+// eyeredClick.onclick = tl1.play
+
+//
+// shapeList.forEach(shape => {
+//   shape.addEventListener('click', (event) => {
+//     event.target.classList.toggle('active')
+//   })
+// });
+// let eyered = anime({
+// targets: '.eyered1',
+// duration: 1000,
+// direction: 'normal',
+// })
+//
+// let eyeredClick =
+// document.querySelector('.eyered1')
+// eyeredClick.onclick = eyered.play
+
+
+// ( document ).ready(function(){
+// 	  ( ".toggle" ).click(function(){ // задаем функцию при нажатиии на элемент с классом toggle
+// 	    ( "div" ).toggle(); //  скрываем, или отображаем все элементы <div>
+// 	  });
+
+
+  // if ( display === true ) {
+  // $( "eyered1" ).show('eyered1');
+  // } else if ( display === false ) {
+  // $( "eyeclose1" ).hide('eyeclose1');
+  // }
